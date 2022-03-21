4b3b4ff50797aeac5656d6a4956268686a70e4c0
A <forgrace5@gmail.com>
2022-03-21 18:41:38 +0500

    first

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-from flask import Flask
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = "Ученик Яндекс.Лицея"
-    return render_template('index.html', title='Домашняя страница',
-                           username=user)
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('base.html', title=title)
 
 
 if __name__ == '__main__':
